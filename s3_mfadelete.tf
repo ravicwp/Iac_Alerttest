265485f3be98c49fa8a1536e3207b1f811743792
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-08-19 18:39:23 +0530

    Update s3_mfadelete.tf

--- s3_mfadelete.tf
+++ s3_mfadelete.tf
@@ -1,56 +1,8 @@
-resource "aws_s3_bucket" "mfadelete" {
+resource "aws_s3_bucket" "ravitesting" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
-    enabled = true
-    mfa_delete = true
+    enabled = false
+    mfa_delete = false
   }
 }
-
-resource "aws_s3_bucket" "mfadelete1" {
-  bucket = "examplebuckettftest1"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete2" {
-  bucket = "examplebuckettftest2"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-
-resource "aws_s3_bucket" "mfadelete3" {
-  bucket = "examplebuckettftest3"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete4" {
-  bucket = "examplebuckettftest4"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-resource "aws_s3_bucket" "mfadelete5" {
-  bucket = "examplebuckettftest5"
-  acl    = "private"
-  versioning {
-    enabled = true
-    mfa_delete = true
-  }
-}
-
-
